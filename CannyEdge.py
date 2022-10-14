32136d0bc37673f4dd2b4aae080954f028112edc
Devraj Priyadarshi <devrajpriyadarshi01@gmail.com>
2022-10-14 20:41:30 +0530

    initialise repo

---
+++ CannyEdge.py
@@ -0,0 +1,28 @@
+import os
+import random
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+
+def CannyEdgeDetection(img):
+    edges = cv2.Canny(img, 100, 200)
+
+    cv2.imshow("img", edges)
+
+if __name__ == "__main__":
+    
+    data_folder = "./data/renders"
+
+    sub_folder = data_folder + "/" + str(random.choice( os.listdir( data_folder)))
+    sub_sub_folder = sub_folder + "/" + str(random.choice( os.listdir( sub_folder)))
+    img_file = sub_sub_folder + "/render_"+ str(random.randint(0,4)) + ".png"
+
+    img = cv2.imread(img_file)
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    CannyEdgeDetection(img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
