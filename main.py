29f08a672801211e2b0f35b0f8c6d82ea53d9865
Devraj Priyadarshi <devrajpriyadarshi01@gmail.com>
2022-12-02 19:32:14 +0530

    changes to git ignore and removed wrong weights

--- main.py
+++ main.py
@@ -12,13 +12,33 @@ from torchsummary import summary
 
 from RepVGG.repvgg import create_RepVGG_A0
 from RepVGG import se_block
+from auxilarynet import AuxilaryBranchCNN, Ensemble
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("device:",device)
+
 
 MainBranch = create_RepVGG_A0().to(device)
 weightsA0 = torch.load("Pretrained_Networks/RepVGG-A0-train.pth")
 MainBranch.load_state_dict(weightsA0)
 
-# print(model)
+yesy = AuxilaryBranchCNN().to(device)
+ensembleBranch = Ensemble(yesy).to(device)
+weights = torch.load("Pretrained_Networks/ensembleModel-01.pth")
+ensembleBranch.load_state_dict(weights)
+
+
+AuxiliaryBranch = AuxilaryBranchCNN().to(device)
+weights = torch.load("Pretrained_Networks/auxiliaryBranch-01.pth")
+AuxiliaryBranch.load_state_dict(weights)
+
+# yesy = AuxilaryBranchCNN().to(device)
+# ensembleBranch = Ensemble(yesy).to(device)
+# weights = torch.load("Pretrained_Networks/auxiliaryBranch-01.pth")
+# ensembleBranch.load_state_dict(weights)
+
+print("---------------- Main Branch ----------------")
 summary(MainBranch, (3, 128,128))
+
+print("---------------- Auxiliary Branch ----------------")
+summary(AuxiliaryBranch, (1, 128,128))
